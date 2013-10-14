29fb3b305b0be9ed8e52eb5246f8f20d19954840
mone <simone.fabiano@gmail.com>
2013-10-14 18:48:52 +0200

    add logging to Stats.js

--- html/Stats.js
+++ html/Stats.js
@@ -1,9 +1,14 @@
 define([],function() {
   
+  var LOG_INTERVAL = 5000;
+  
   var Stats = function() {
     this.div = document.createElement("div");
     document.body.appendChild(this.div);
     
+    this.lastLog = null;
+    this.hz = null;
+    this.count = 0;
     
   };
   
@@ -11,7 +16,29 @@ define([],function() {
     
     timestamp: function(ts) {
       this.div.innerHTML = ts;
+      this.count++;
       
+      
+      var now = new Date().getTime();
+      if (this.lastLog) {
+        if (now-this.lastLog > LOG_INTERVAL) {
+          if (this.hz) {
+            var expected = (LOG_INTERVAL / 1000) * this.hz;
+            if (this.count != expected) {
+              console.log("Received " + this.count + " in the last " + LOG_INTERVAL + " millis (was expecting " + expected + ")");
+            } else {
+              console.log("Received " + this.count + " in the last " + LOG_INTERVAL + " millis (as expected)");
+            }
+          } else {
+            console.log("Received " + this.count + " in the last " + LOG_INTERVAL + " millis");
+          }
+          
+          this.lastLog = now;
+          this.count = 0;
+        }
+      } else {
+        this.lastLog = now;
+      }
       
       
     },
